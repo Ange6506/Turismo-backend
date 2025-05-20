d73844a26224d2056748d74c5a93856297934894
Angélica <angelicacb6392@gmail.com>
2025-05-20 11:07:09 -0500

    Configuración .env para conexión a base de datos Railway y arreglos de conexión

--- Controller/Register_Admin.js
+++ Controller/Register_Admin.js
@@ -5,14 +5,14 @@ exports.Register_Admin = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
-  const { username, contraseña, rol_id } = req.body;
+  const { username, contrasena, rol_id } = req.body;
 
   if (!rol_id) {
     return res.status(400).json({ message: 'Rol no proporcionado' });
   }
 
-  const sql = 'INSERT INTO usuarios (nombre_usuario, contraseña, rol_id) VALUES (?, ?, ?)';
-  db.query(sql, [username, contraseña, rol_id], (err, result) => {
+  const sql = 'INSERT INTO usuarios (nombre_usuario, contrasena, rol_id) VALUES (?, ?, ?)';
+  db.query(sql, [username, contrasena, rol_id], (err, result) => {
     if (err) {
       console.error(err);  // Registrar el error
       return res.status(500).json({ message: 'Error al registrar', error: err });
