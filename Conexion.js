0904414de982e373ab7a1c52c711ce6bcda96e77
Angélica <angelicacb6392@gmail.com>
2025-05-20 11:45:18 -0500

    env

--- Conexion.js
+++ Conexion.js
@@ -3,9 +3,11 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT), // IMPORTANTE pasar a número
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 10000, // 10 segundos
 });
 
 db.connect(err => {
